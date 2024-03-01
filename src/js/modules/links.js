095b2ff3d6d89e26196fbef02221b1ecf7b598ee
Warling-Sebastian <150879739+Warling-Sebastian@users.noreply.github.com>
2024-03-01 03:26:15 +0100

    app
    good

--- src/js/modules/links.js
+++ src/js/modules/links.js
@@ -11,11 +11,16 @@ function app_Enter_Register(){
     const password_confirm = document.getElementById("register-Password-C").value;
     const grade = document.getElementById("register-Grade").value;
     const section = document.getElementById("register-Section").value;
-    localStorage.setItem("name", nme)
-    localStorage.setItem("lastName", lastName)
-    localStorage.setItem("password", password)
-    localStorage.setItem("password_C", password_confirm) 
-    localStorage.setItem("grade", grade)
-    localStorage.setItem("section", section)
-    window.location.href="/src/views/apk.html" 
+    if (password == password_confirm) {
+        localStorage.setItem("name", nme)
+        localStorage.setItem("lastName", lastName)
+        localStorage.setItem("password", password)
+        localStorage.setItem("password_C", password_confirm) 
+        localStorage.setItem("grade", grade)
+        localStorage.setItem("section", section)
+        window.location.href="/src/views/apk.html"
+    }else{
+     let x = document.getElementById('test_password')
+        x.innerHTML= `<p>La contraseña no es igual a la primera </p>`
+    }
 }
--- src/js/modules/register-data.js
+++ src/js/modules/register-data.js
@@ -5,7 +5,28 @@ let password_apk = localStorage.getItem('password')
 let password_Capk = localStorage.getItem('password_C')
 let grado = localStorage.getItem('grade')
 let seccion = localStorage.getItem('section')
+let name_complete = nombre+apellido;
+document.querySelector('.usuario').innerHTML = `${nombre}`;
+document.querySelector('#menu_user').innerHTML = `${nombre}`;
 
-document.getElementById('usuario').innerHTML = `${nombre}`;
-
-    
+function notify() {
+    // Verificar si el navegador soporta las notificaciones
+if ("Notification" in window) {
+    // Pedir permiso para enviar notificaciones
+    Notification.requestPermission().then(function(permission) {
+      if (permission === "granted") {
+        // Crear la notificación
+        var notification = new Notification("!", {
+          body: "klk kev esta es una noty por ejemplos"
+        });
+  
+        // Mostrar la notificación
+        setTimeout(notification.close.bind(notification), 5000);
+      } else {
+        console.log("El usuario denegó el permiso para enviar notificaciones.");
+      }
+    });
+  } else {
+    console.log("Este navegador no soporta las notificaciones.");
+  }
+}
